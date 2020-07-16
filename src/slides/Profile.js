db49a1cb2e1fb17d2555d4d1161527dbb3a561ee
Henry Paulino <henrypl360@gmail.com>
2020-07-16 16:33:41 -0400

    add animation

--- src/slides/Profile.js
+++ src/slides/Profile.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Lottie from 'lottie-react-web'
+import profileAnimation from '../assets/profile_animation.json'
 
-export default () => (
-  <div className="slide">
-    <h1>
-      I'm Henry, <br />
-      React Native Developer
-    </h1>
-    <p className="sub-text">Front End Developer / Mobile Developer</p>
-  </div>
-)
+export default () => {
+
+  return (
+    <div className="row slide">
+      <div className="col">
+        <h1>
+          I'm Henry, <br />
+          React Native Developer
+        </h1>
+        <p className="sub-text">Front End Developer / Mobile Developer</p>
+      </div>
+      <Lottie options={{ animationData: profileAnimation }} height="55%" width="55%" />
+    </div>
+  )
+}
