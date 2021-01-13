c332b7b43c637770443afcf4e861226e5a684a72
Henry Paulino <henrypl360@gmail.com>
2021-01-13 00:13:28 -0500

    add about me page

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+const About = () => (
+  <div className="flex justify-center bg-white py-16 px-14 flex-col shadow-lg">
+    <span className="text-6xl font-black">Hello, I am</span>
+    <span className="text-6xl font-black text-accent">Henry Paulino</span>
+
+    <p className="text-2xl mt-8">I am a Front-End Developer and React Native Developer.</p>
+
+    <div className="border-b-2 border-accent w-48 pb-1 mt-8">
+      <a className="text-2xl text-accent font-extrabold inline">See my projects</a>
+    </div>
+  </div>
+)
+
+export default About
