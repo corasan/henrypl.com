a3a8ced32a659f118eddec2703595673ca92df0e
Henry Paulino <henrypl360@gmail.com>
2021-01-13 02:07:45 -0500

    extra link to view my resume

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,15 +4,28 @@ const About = () => (
     <span className="text-6xl font-black text-accent">Henry Paulino</span>
 
     <p className="text-2xl mt-8">
-      I am a Front-End Developer and React Native Developer. From time to time I play around in the
-      Back-End using Node.js and a little Ruby on Rails.
+      I am a Front-End Developer and React Native Developer. From time to time I like play around in
+      the Back-End using Node.js and a little Ruby on Rails.
     </p>
 
     <div>
       <div className="inline-block border-b-2 border-accent pb-1 mt-8">
-        <a className="text-2xl text-accent font-extrabold inline cursor-pointer">See my projects</a>
+        <a
+          href="https://drive.google.com/file/d/1N5ZhlxJspzOvq2x-hzBEysx9WdTm7Lfe/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+          className="text-2xl text-accent font-extrabold inline cursor-pointer"
+        >
+          Resume
+        </a>
       </div>
     </div>
+
+    {/* <div>
+      <div className="inline-block border-b-2 border-accent pb-1 mt-8">
+        <a className="text-2xl text-accent font-extrabold inline cursor-pointer">See my projects</a>
+      </div>
+    </div> */}
   </div>
 )
 
