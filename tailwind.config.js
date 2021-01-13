f0b7009c1e35121349f256ff0da7160933a2abe6
Henry Paulino <henrypl360@gmail.com>
2021-01-12 22:59:46 -0500

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      black: colors.black,
+      emerald: colors.emerald,
+      accent: '#00E8B6',
+    },
   },
   variants: {
     extend: {},
