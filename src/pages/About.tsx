b7e55f475e7102d7cb7604e62a238e7e89d2c9e6
Henry Paulino <henrypl360@gmail.com>
2021-01-20 19:39:54 -0500

    responsive about section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,32 +1,37 @@
-const About = () => (
-  <div className="flex flex-wrap w-3/5 justify-center bg-white py-16 px-14 flex-col shadow-lg">
-    <span className="text-6xl font-black">Hello, I am</span>
-    <span className="text-6xl font-black text-accent">Henry Paulino</span>
+const About = () => {
+  const responsive = 'sm:w-3/5 sm:px-14 sm:py-16 sm:shadow-lg'
+  return (
+    <div
+      className={`flex flex-wrap min-w-full sm:min-w-0 justify-center bg-white pb-24 px-0 flex-col ${responsive}`}
+    >
+      <span className="text-4xl font-black sm:text-6xl">Hello, I am</span>
+      <span className="text-4xl font-black text-accent sm:text-6xl">Henry Paulino</span>
 
-    <p className="text-2xl mt-8">
-      I am a Front-End Developer and React Native Developer. From time to time I like play around in
-      the Back-End using Node.js or Ruby on Rails.
-    </p>
+      <p className="text-xl mt-8 sm:text-2xl">
+        I am a Front-End Developer and React Native Developer. From time to time I like play around
+        in the Back-End using Node.js or Ruby on Rails.
+      </p>
 
-    <div>
-      <div className="inline-block border-b-2 border-accent pb-1 mt-8">
-        <a
-          href="https://drive.google.com/file/d/1N5ZhlxJspzOvq2x-hzBEysx9WdTm7Lfe/view?usp=sharing"
-          target="_blank"
-          rel="noreferrer"
-          className="text-2xl text-accent font-extrabold inline cursor-pointer"
-        >
-          Resume
-        </a>
+      <div>
+        <div className="inline-block border-b-2 border-accent pb-1 mt-8">
+          <a
+            href="https://drive.google.com/file/d/1N5ZhlxJspzOvq2x-hzBEysx9WdTm7Lfe/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+            className="text-xl sm:text-2xl text-accent font-extrabold inline cursor-pointer"
+          >
+            Resume
+          </a>
+        </div>
       </div>
-    </div>
 
-    {/* <div>
+      {/* <div>
       <div className="inline-block border-b-2 border-accent pb-1 mt-8">
         <a className="text-2xl text-accent font-extrabold inline cursor-pointer">See my projects</a>
       </div>
     </div> */}
-  </div>
-)
+    </div>
+  )
+}
 
 export default About
