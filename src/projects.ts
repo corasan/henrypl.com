ff3a931c47289e479c5a79b0d3e4b3b20ebec311
Henry Paulino <henrypl360@gmail.com>
2021-01-31 18:07:18 -0500

    add projects object

---
+++ src/projects.ts
@@ -0,0 +1,48 @@
+export default {
+  projects: [
+    {
+      name: 'Stocket',
+      description: 'Stock Market simulator built with React Native and IEX Cloud API.',
+      urls: [
+        {
+          title: 'Website',
+          url: 'https://www.stocketapp.com/',
+        },
+        {
+          title: 'App Store',
+          url: 'https://apps.apple.com/us/app/stocket-app/id1493392383',
+        },
+      ],
+      image: `${process.env.PUBLIC_URL}/assets/stocket.png`,
+      tags: ['SwiftUI'],
+    },
+    {
+      name: 'E-Commerce App',
+      description: 'E-commerce simple app to learn SwiftUI.',
+      urls: [
+        {
+          title: 'Github',
+          url: 'https://github.com/corasan/ecommerce-swiftui',
+        },
+      ],
+      image: `${process.env.PUBLIC_URL}/assets/ecommerce.png`,
+      tags: ['SwiftUI'],
+    },
+    {
+      name: 'CloudMate',
+      description: 'Stock Market simulator built with React Native and IEX Cloud API.',
+      urls: [
+        {
+          title: 'Github',
+          url: 'https://github.com/corasan/weather-app',
+        },
+        {
+          title: 'App Store',
+          url: 'https://apps.apple.com/us/app/cloudmate/id1470349168',
+        },
+      ],
+      image: `${process.env.PUBLIC_URL}/assets/cloudmate.png`,
+      tags: ['SwiftUI'],
+    },
+  ],
+}
